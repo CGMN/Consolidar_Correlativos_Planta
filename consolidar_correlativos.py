4bd1d6d92660a835e4a0c9d8e4d897935b30f89f
CGMN <hadesis@gmail.com>
2018-11-26 17:48:25 -0300

    Create consolidar_correlativos.py

---
+++ consolidar_correlativos.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import glob
+import os
+import os.path
+
+pasos=7
+
+print('')
+print('Paso 1 de '+str(pasos)+'- Borrando consolidado anterior')
+if os.path.isfile("consolidado_correlativos.csv"):
+    os.remove("consolidado_correlativos.csv")
+
+archivostxt=glob.glob("*.txt")
+
+print('Paso 2 de '+str(pasos)+'- Leyendo archivos')
+
+archivos_correlativos=[]
+for i in archivostxt:
+	archivos_correlativos.append(pd.read_csv(i,encoding='latin1',sep=';', low_memory=False))
+
+print ('             Hay un total de '+str(len(archivos_correlativos))+' archivos')
+#extraer los primeros 3 caracteres del nombre
+print('Paso 3 de '+str(pasos)+'- Extrayendo codigos')
+codigos=[]
+for i in range(0,len(archivostxt)):
+	codigos.append(str(archivostxt[i][0:3]))
+
+#pegar los primeros 3 caracteres del nombre en la ultima columna de cada archivo
+
+print('Paso 4 de '+str(pasos)+'- Escribiendo codigos')
+for j in range(0,len(archivos_correlativos)):
+    archivos_correlativos[j].loc[0,"SERVICIO"]=""
+    print('             Servicio: '+str(codigos[j]))
+    for i in range(0, len(archivos_correlativos[j])):
+        archivos_correlativos[j].loc[i,"SERVICIO"]=codigos[j]
+
+
+print('Paso 5 de '+str(pasos)+'- Concatenando archivos')
+
+#consolid a
+consolidado=pd.concat(archivos_correlativos)
+consolidado=pd.concat(archivos_correlativos).reset_index(drop = True)
+
+
+#creo que aca pongo la ultima columna en la primera posicion
+cols = [consolidado.columns[-1]] + [col for col in consolidado if col != consolidado.columns[-1]]
+consolidado = consolidado[cols]
+
+print ('Paso 6 de '+str(pasos)+'- Grabando Archivo')
+consolidado.to_csv('consolidado_correlativos.csv', encoding='latin1',index=False)
+
+print('Paso 7 de '+str(pasos)+'- Archivo listo')
